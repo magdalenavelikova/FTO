f739d7d441eb3f34ce8bb3dcf578d49bee16cfed
magdalenavelikova <magibox@yahoo.com>
2024-03-22 14:46:15 +0200

    add testImplementation "com.redis:testcontainers-redis:2.2.0" for clean build

--- rest-server/src/main/java/com/fto/model/dto/UserDto.java
+++ rest-server/src/main/java/com/fto/model/dto/UserDto.java
@@ -7,7 +7,6 @@ public class UserDto {
     private Long id;
     private String email;
     private String name;
-
     private String created;
     private List<UserRoleDto> roles = new ArrayList<>();
 
--- rest-server/src/test/java/com/fto/FtoApplicationTests.java
+++ rest-server/src/test/java/com/fto/FtoApplicationTests.java
@@ -2,8 +2,10 @@ package com.fto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootTest
+@EnableCaching
 class FtoApplicationTests {
 
 	@Test
